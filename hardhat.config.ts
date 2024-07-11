16982d063b796d2fc89f8f727e2d21672da81beb
Jakub Kobeldys <jakub@Ratchet.local>
2024-07-11 09:57:28 +0200

    Import Tenderly Hardhat plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as tenderly from "@tenderly/hardhat-tenderly";
+import "dotenv/config";
+import { HardhatUserConfig } from "hardhat/config";
+ 
+tenderly.setup({ automaticVerifications: true });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    virtualMainnet: {
+      url: process.env.TENDERLY_VIRTUAL_MAINNET_RPC!,
+    },
+  },
+  tenderly: {
+    // https://docs.tenderly.co/account/projects/account-project-slug
+    project: process.env.TENDERLY_PROJECT!,
+    username: process.env.TENDERLY_USERNAME!,
+  },
 };
 
 export default config;
