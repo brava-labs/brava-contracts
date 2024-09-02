e991b021e994e15dd9c4f840433a0aeedd1d3e73
ba1ciu <jakub@amorphic.xyz>
2024-09-02 09:10:19 +0200

    Lint replace fix

--- contracts/interfaces/fluid/IFToken.sol
+++ contracts/interfaces/fluid/IFToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity^0.8.0;
+pragma solidity ^0.8.0;
 
 interface IFToken {
     function previewDeposit(uint256 _assets) external view returns (uint256);
