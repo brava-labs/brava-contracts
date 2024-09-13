fa98f70751efd9278bda35d873d63c9c406edf5a
Splidge <73956628+Splidge@users.noreply.github.com>
2024-09-13 16:14:33 +0100

    fix missed solhint fixes

--- contracts/libraries/SafeUIntCast.sol
+++ contracts/libraries/SafeUIntCast.sol
@@ -3,11 +3,15 @@
 pragma solidity =0.8.24;
 
 library SafeUIntCast {
+    error ValueExceedsRangeOfUint(uint8 size);
+
     /// @notice Convers a uint256 to uint128, reverts if the value is out of range
     /// @param value The uint256 value to be converted
     /// @return The converted uint128 value
     function toUint128(uint256 value) internal pure returns (uint128) {
-        require(value <= type(uint128).max, "Value exceeds uint128 range");
+        if (value > type(uint128).max) {
+            revert ValueExceedsRangeOfUint(128);
+        }
         return uint128(value);
     }
 
@@ -15,7 +19,9 @@ library SafeUIntCast {
     /// @param value The uint256 value to be converted
     /// @return The converted uint96 value
     function toUint96(uint256 value) internal pure returns (uint96) {
-        require(value <= type(uint96).max, "Value exceeds uint96 range");
+        if (value > type(uint96).max) {
+            revert ValueExceedsRangeOfUint(96);
+        }
         return uint96(value);
     }
 
@@ -23,15 +29,29 @@ library SafeUIntCast {
     /// @param value The uint256 value to be converted
     /// @return The converted uint64 value
     function toUint64(uint256 value) internal pure returns (uint64) {
-        require(value <= type(uint64).max, "Value exceeds uint64 range");
+        if (value > type(uint64).max) {
+            revert ValueExceedsRangeOfUint(64);
+        }
         return uint64(value);
+    }
+
+    /// @notice Converts a uint256 to uint40, reverts if the value is out of range
+    /// @param value The uint256 value to be converted
+    /// @return The converted uint40 value
+    function toUint40(uint256 value) internal pure returns (uint40) {
+        if (value > type(uint40).max) {
+            revert ValueExceedsRangeOfUint(40);
+        }
+        return uint40(value);
     }
 
     /// @notice Converts a uint256 to uint32, reverts if the value is out of range
     /// @param value The uint256 value to be converted
     /// @return The converted uint32 value
     function toUint32(uint256 value) internal pure returns (uint32) {
-        require(value <= type(uint32).max, "Value exceeds uint32 range");
+        if (value > type(uint32).max) {
+            revert ValueExceedsRangeOfUint(32);
+        }
         return uint32(value);
     }
 
@@ -39,7 +59,9 @@ library SafeUIntCast {
     /// @param value The uint256 value to be converted
     /// @return The converted uint24 value
     function toUint24(uint256 value) internal pure returns (uint24) {
-        require(value <= type(uint24).max, "Value exceeds uint24 range");
+        if (value > type(uint24).max) {
+            revert ValueExceedsRangeOfUint(24);
+        }
         return uint24(value);
     }
 
@@ -47,7 +69,9 @@ library SafeUIntCast {
     /// @param value The uint256 value to be converted
     /// @return The converted uint16 value
     function toUint16(uint256 value) internal pure returns (uint16) {
-        require(value <= type(uint16).max, "Value exceeds uint16 range");
+        if (value > type(uint16).max) {
+            revert ValueExceedsRangeOfUint(16);
+        }
         return uint16(value);
     }
 
@@ -55,15 +79,9 @@ library SafeUIntCast {
     /// @param value The uint256 value to be converted
     /// @return The converted uint8 value
     function toUint8(uint256 value) internal pure returns (uint8) {
-        require(value <= type(uint8).max, "Value exceeds uint8 range");
+        if (value > type(uint8).max) {
+            revert ValueExceedsRangeOfUint(8);
+        }
         return uint8(value);
-    }
-
-    /// @notice Converts a uint256 to uint40, reverts if the value is out of range
-    /// @param value The uint256 value to be converted
-    /// @return The converted uint40 value
-    function toUint40(uint256 value) internal pure returns (uint40) {
-        require(value <= type(uint40).max, "Value exceeds uint40 range");
-        return uint40(value);
     }
 }
