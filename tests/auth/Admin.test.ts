8613f33929818c5ad23a91f2d45385b22a9aef35
Splidge <73956628+Splidge@users.noreply.github.com>
2024-09-27 11:36:35 +0100

    add fee taking test

--- tests/auth/Admin.test.ts
+++ tests/auth/Admin.test.ts
@@ -1,9 +1,17 @@
 import { ethers, network } from 'hardhat';
 import { expect } from 'chai';
-import { AdminVault, Logger, IERC20 } from '../../typechain-types';
+import { AdminVault, IERC20, IFluidLending, FluidSupply } from '../../typechain-types';
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
-import { getUSDC } from '../utils-stable';
-import { log, deploy } from '../utils';
+import { getUSDC, fundAccountWithToken } from '../utils-stable';
+import {
+  log,
+  deploy,
+  getBaseSetup,
+  getDeployedContract,
+  calculateExpectedFee,
+  executeAction,
+} from '../utils';
+import { tokenConfig } from '../constants';
 
 describe('AdminVault', function () {
   let adminVault: AdminVault;
@@ -14,135 +22,191 @@ describe('AdminVault', function () {
   let carol: SignerWithAddress;
   let snapshotId: string;
   let USDC: IERC20;
+  let safeAddr: string;
+  let fUSDC: IFluidLending;
+  describe('Direct tests', function () {
+    before(async () => {
+      [admin, owner, alice, bob, carol] = await ethers.getSigners();
 
-  before(async () => {
-    [admin, owner, alice, bob, carol] = await ethers.getSigners();
+      adminVault = await deploy('AdminVault', admin, owner.address, admin.address);
+      // Fetch the USDC token
+      USDC = await getUSDC();
 
-    adminVault = await deploy('AdminVault', admin, owner.address, admin.address);
-    // Fetch the USDC token
-    USDC = await getUSDC();
+      // Take local snapshot before running tests
+      log('Taking local snapshot');
+      snapshotId = await network.provider.send('evm_snapshot');
+    });
 
-    // Take local snapshot before running tests
-    log('Taking local snapshot');
-    snapshotId = await network.provider.send('evm_snapshot');
+    beforeEach(async () => {});
+
+    afterEach(async () => {
+      // Revert local snapshot after each test
+      log('Reverting to local snapshot');
+      await network.provider.send('evm_revert', [snapshotId]);
+
+      // IMPORTANT: take a new snapshot, they can't be reused!
+      snapshotId = await network.provider.send('evm_snapshot');
+    });
+
+    // tests that can be run directly on the contract
+    it('should set owner correctly', async function () {
+      await expect(
+        adminVault.connect(alice).changeOwner(alice.address)
+      ).to.be.revertedWithCustomError(adminVault, 'SenderNotOwner');
+
+      await adminVault.connect(owner).changeOwner(alice.address);
+      expect(await adminVault.owner()).to.equal(alice.address);
+    });
+
+    it('should set admin correctly', async function () {
+      await expect(
+        adminVault.connect(alice).changeAdmin(alice.address)
+      ).to.be.revertedWithCustomError(adminVault, 'SenderNotAdmin');
+      // try as owner
+      await adminVault.connect(owner).changeAdmin(alice.address);
+      expect(await adminVault.admin()).to.equal(alice.address);
+
+      // try as admin
+      await adminVault.connect(alice).changeAdmin(bob.address);
+      expect(await adminVault.admin()).to.equal(bob.address);
+    });
+
+    it('should set fee recipient correctly', async function () {
+      await expect(
+        adminVault.connect(alice).setFeeRecipient(alice.address)
+      ).to.be.revertedWithCustomError(adminVault, 'SenderNotOwner');
+
+      await expect(
+        adminVault.connect(owner).setFeeRecipient(ethers.ZeroAddress)
+      ).to.be.revertedWithCustomError(adminVault, 'InvalidRecipient');
+
+      await adminVault.connect(owner).setFeeRecipient(alice.address);
+      expect(await adminVault.feeRecipient()).to.equal(alice.address);
+    });
+
+    it('should set fee percentage correctly', async function () {
+      await expect(adminVault.connect(alice).setFeeRange(100, 200)).to.be.revertedWithCustomError(
+        adminVault,
+        'SenderNotAdmin'
+      );
+
+      await expect(adminVault.connect(owner).setFeeRange(200, 100)).to.be.revertedWithCustomError(
+        adminVault,
+        'InvalidRange'
+      );
+
+      await adminVault.connect(owner).setFeeRange(100, 200);
+      expect(await adminVault.minFeeBasis()).to.equal(100);
+      expect(await adminVault.maxFeeBasis()).to.equal(200);
+    });
+    it('should initialize fee timestamp correctly', async function () {
+      const tx = await adminVault.connect(owner).initializeFeeTimestamp(alice.address);
+
+      const receipt = await tx.wait();
+      const blockTimestamp = (await ethers.provider.getBlock(receipt!.blockNumber))!.timestamp;
+      expect(await adminVault.lastFeeTimestamp(owner.address, alice.address)).to.equal(
+        blockTimestamp
+      );
+    });
+
+    it('should update fee timestamp correctly', async function () {
+      await adminVault.connect(owner).initializeFeeTimestamp(alice.address);
+      const tx = await adminVault.connect(owner).updateFeeTimestamp(alice.address);
+      const receipt = await tx.wait();
+      const blockTimestamp = (await ethers.provider.getBlock(receipt!.blockNumber))!.timestamp;
+      expect(await adminVault.lastFeeTimestamp(owner.address, alice.address)).to.equal(
+        blockTimestamp
+      );
+    });
   });
+  // tests that need to be run via inheritance
+  // lets deploy an action contract and test via that
+  describe('Indirect tests', function () {
+    let signer: SignerWithAddress;
+    let loggerAddress: string;
+    let fUSDC: IFluidLending;
+    let fluidSupplyContract: FluidSupply;
+    let fluidSupplyAddress: string;
+    before(async () => {
+      [signer] = await ethers.getSigners();
+      const baseSetup = await getBaseSetup(signer);
+      if (!baseSetup) {
+        throw new Error('Base setup not deployed');
+      }
+      safeAddr = (await baseSetup.safe.getAddress()) as string;
+      adminVault = await baseSetup.adminVault;
+      loggerAddress = (await baseSetup.logger.getAddress()) as string;
+      // Fetch the USDC token
+      USDC = await getUSDC();
 
-  beforeEach(async () => {});
+      // Initialize FluidSupply and FluidWithdraw actions
+      fluidSupplyContract = await deploy(
+        'FluidSupply',
+        signer,
+        await baseSetup.contractRegistry.getAddress(),
+        loggerAddress,
+        await adminVault.getAddress()
+      );
+      fluidSupplyAddress = await fluidSupplyContract.getAddress();
+      fUSDC = await ethers.getContractAt('IFluidLending', tokenConfig.fUSDC.address);
+    });
+    it('should calculate fee correctly for a given period', async function () {
+      const token = 'USDC';
+      const amount = ethers.parseUnits('100', tokenConfig[token].decimals);
 
-  afterEach(async () => {
-    // Revert local snapshot after each test
-    log('Reverting to local snapshot');
-    await network.provider.send('evm_revert', [snapshotId]);
+      await fundAccountWithToken(safeAddr, token, amount);
 
-    // IMPORTANT: take a new snapshot, they can't be reused!
-    snapshotId = await network.provider.send('evm_snapshot');
-  });
+      const feeRecipient = await adminVault.feeRecipient();
+      const feeRecipientUSDCBalanceBefore = await USDC.balanceOf(feeRecipient);
+      const feeRecipientfUSDCBalanceBefore = await fUSDC.balanceOf(feeRecipient);
 
-  it('should set owner correctly', async function () {
-    await expect(
-      adminVault.connect(alice).changeOwner(alice.address)
-    ).to.be.revertedWithCustomError(adminVault, 'SenderNotOwner');
+      const supplyTx = await executeAction({
+        type: 'FluidSupply',
+        amount,
+      });
 
-    await adminVault.connect(owner).changeOwner(alice.address);
-    expect(await adminVault.owner()).to.equal(alice.address);
-  });
+      const fUSDCBalanceAfterSupply = await fUSDC.balanceOf(safeAddr);
 
-  it('should set admin correctly', async function () {
-    await expect(
-      adminVault.connect(alice).changeAdmin(alice.address)
-    ).to.be.revertedWithCustomError(adminVault, 'SenderNotAdmin');
-    // try as owner
-    await adminVault.connect(owner).changeAdmin(alice.address);
-    expect(await adminVault.admin()).to.equal(alice.address);
+      const initialFeeTimestamp = await adminVault.lastFeeTimestamp(
+        safeAddr,
+        tokenConfig.fUSDC.address
+      );
+      const finalFeeTimestamp = initialFeeTimestamp + BigInt(60 * 60 * 24 * 365); // add 1 year to the initial timestamp
 
-    // try as admin
-    await adminVault.connect(alice).changeAdmin(bob.address);
-    expect(await adminVault.admin()).to.equal(bob.address);
-  });
+      // now time travel like you're Dr Emmett Brown
+      await network.provider.send('evm_setNextBlockTimestamp', [finalFeeTimestamp.toString()]);
 
-  it('should set fee recipient correctly', async function () {
-    await expect(
-      adminVault.connect(alice).setFeeRecipient(alice.address)
-    ).to.be.revertedWithCustomError(adminVault, 'SenderNotOwner');
+      const withdrawTx = await executeAction({
+        type: 'FluidSupply',
+        token,
+        feePercentage: 10,
+        amount: '0',
+      });
 
-    await expect(
-      adminVault.connect(owner).setFeeRecipient(ethers.ZeroAddress)
-    ).to.be.revertedWithCustomError(adminVault, 'InvalidRecipient');
+      const expectedFee = await calculateExpectedFee(
+        supplyTx,
+        withdrawTx,
+        10,
+        fUSDCBalanceAfterSupply
+      );
+      const expectedFeeRecipientBalance = feeRecipientfUSDCBalanceBefore + expectedFee;
 
-    await adminVault.connect(owner).setFeeRecipient(alice.address);
-    expect(await adminVault.feeRecipient()).to.equal(alice.address);
-  });
+      // don't take fees in the underlying asset
+      expect(await USDC.balanceOf(feeRecipient)).to.equal(feeRecipientUSDCBalanceBefore);
+      // take fees in the fToken
+      expect(await fUSDC.balanceOf(feeRecipient)).to.equal(expectedFeeRecipientBalance);
+    });
 
-  it('should set fee percentage correctly', async function () {
-    await expect(adminVault.connect(alice).setFeeRange(100, 200)).to.be.revertedWithCustomError(
-      adminVault,
-      'SenderNotAdmin'
-    );
+    it.skip('should recover ETH correctly', async function () {
+      // We should consider removing the withdrawStuckFunds function
+      // as it could pose a security risk to users funds
+      // should it be executed using a delegate call on a safe
+    });
 
-    await expect(adminVault.connect(owner).setFeeRange(200, 100)).to.be.revertedWithCustomError(
-      adminVault,
-      'InvalidRange'
-    );
-
-    await adminVault.connect(owner).setFeeRange(100, 200);
-    expect(await adminVault.minFeeBasis()).to.equal(100);
-    expect(await adminVault.maxFeeBasis()).to.equal(200);
-  });
-  it('should initialize fee timestamp correctly', async function () {
-    const tx = await adminVault.connect(owner).initializeFeeTimestamp(alice.address);
-
-    const receipt = await tx.wait();
-    const blockTimestamp = (await ethers.provider.getBlock(receipt!.blockNumber))!.timestamp;
-    expect(await adminVault.lastFeeTimestamp(owner.address, alice.address)).to.equal(
-      blockTimestamp
-    );
-  });
-
-  it('should update fee timestamp correctly', async function () {
-    await adminVault.connect(owner).initializeFeeTimestamp(alice.address);
-    const tx = await adminVault.connect(owner).updateFeeTimestamp(alice.address);
-    const receipt = await tx.wait();
-    const blockTimestamp = (await ethers.provider.getBlock(receipt!.blockNumber))!.timestamp;
-    expect(await adminVault.lastFeeTimestamp(owner.address, alice.address)).to.equal(
-      blockTimestamp
-    );
-  });
-
-  it('should calculate fee correctly for a given period', async function () {
-    await adminVault.connect(owner).setFeeRange(100, 200);
-    expect(await adminVault.minFeeBasis()).to.equal(100);
-    expect(await adminVault.maxFeeBasis()).to.equal(200);
-  });
-
-  it.skip('should withdraw fees correctly', async function () {
-    await adminVault.setFeeRecipient(user.address);
-    await mockToken.mint(adminVault.getAddress(), ethers.parseEther('100'));
-
-    await expect(
-      adminVault.connect(user).withdrawFees(mockToken.getAddress(), ethers.parseEther('50'))
-    ).to.be.revertedWith('Caller is not the admin');
-
-    await adminVault.connect(owner).withdrawFees(mockToken.getAddress(), ethers.parseEther('50'));
-    expect(await mockToken.balanceOf(user.address)).to.equal(ethers.parseEther('50'));
-  });
-
-  it.skip('should recover ETH correctly', async function () {
-    const initialBalance = await ethers.provider.getBalance(owner.address);
-    await adminVault
-      .connect(owner)
-      .withdrawStuckFunds(ethers.ZeroAddress, owner.address, ethers.parseEther('100'));
-    const finalBalance = await ethers.provider.getBalance(owner.address);
-    expect(finalBalance).to.equal(initialBalance + ethers.parseEther('100'));
-  });
-
-  it.skip('should recover ERC20 tokens correctly', async function () {
-    await mockToken.mint(adminVault.getAddress(), ethers.parseEther('100'));
-
-    await expect(
-      adminVault.connect(user).recoverERC20(mockToken.getAddress(), ethers.parseEther('50'))
-    ).to.be.revertedWith('Ownable: caller is not the owner');
-
-    await adminVault.recoverERC20(mockToken.getAddress(), ethers.parseEther('50'));
-    expect(await mockToken.balanceOf(owner.address)).to.equal(ethers.parseEther('50'));
+    it.skip('should recover ERC20 tokens correctly', async function () {
+      // We should consider removing the withdrawStuckFunds function
+      // as it could pose a security risk to users funds
+      // should it be executed using a delegate call on a safe
+    });
   });
 });
