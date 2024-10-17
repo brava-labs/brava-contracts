be573d55f49256c61350094b88917ce78133c917
Splidge <73956628+Splidge@users.noreply.github.com>
2024-10-17 14:39:38 +0100

    implement `changeDelay` logic

--- contracts/auth/AccessControlDelayed.sol
+++ contracts/auth/AccessControlDelayed.sol
@@ -7,8 +7,9 @@ import {Errors} from "../Errors.sol";
 
 /// @title Add delays to granting roles in access control
 abstract contract AccessControlDelayed is AccessControl {
-    uint256 public delay;
-    uint256 public delayReductionLockTime;
+    uint256 public delay; // How long after a proposal can the role be granted
+    uint256 public proposedDelay; // New delay to be set after delayReductionLockTime
+    uint256 public delayReductionLockTime; // Time when the new delay can be set/used
     // mapping of proposed roles to the timestamp they can be granted
     mapping(bytes32 => uint256) public proposedRoles;
 
@@ -59,14 +60,8 @@ abstract contract AccessControlDelayed is AccessControl {
         if (proposedRoles[proposalId] > 0) {
             revert Errors.AdminVault_AlreadyProposed();
         }
-        uint256 additionalDelay = 0;
-        // Check if delay reduction lock time is passed
-        if (block.timestamp < delayReductionLockTime) {
-            // lock time is not passed, add additional delay
-            additionalDelay = delayReductionLockTime - block.timestamp;
-        }
-        // add to list of proposed roles using block.timestamp + delay + additionalDelay(if any)
-        proposedRoles[proposalId] = block.timestamp + delay + additionalDelay;
+        // add to list of proposed roles with the wait time
+        proposedRoles[proposalId] = _getDelayTimestamp();
     }
 
     // A helper to find the time when a role proposal will be available to grant
@@ -74,25 +69,63 @@ abstract contract AccessControlDelayed is AccessControl {
         return proposedRoles[keccak256(abi.encodePacked(role, account))];
     }
 
-    function changeDelay(uint256 newDelay) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        // delay must be different and not more than 5 days (to avoid costly mistakes)
-        if (newDelay == delay || newDelay > 5 days) {
+    // Admin function to change the delay
+    // If the new delay is longer we just use it.
+    // If the new delay is shorter we must set a timestamp for when the old delay
+    // would have expired and we can use the new delay after that time.
+    // e.g. If the delay is 2 hours, and we reduce it to 1 hour. All new proposals
+    //      must wait until at least now + 2 hours (old delay) but in 1 hour's time
+    //      they may start using the new delay (because both the old and the new
+    //      delays will have passed by the time they may be granted).
+    // Note: We don't simply add the shorter delay to the delayReductionLockTime
+    //       because for legitimate use we may want to shorten the delay, say from
+    //       2 days to 1 day, in this case we don't want to wait a total of 3 days.
+    // This means that the delay used by default should include enough time to:
+    //   -- Notice the change
+    //   -- Deal with the security hole (remove attackers permissions)
+    //   -- Adjust the delay back to a suitable value
+    //   -- Cancel any proposals made during this period
+    function changeDelay(uint256 _newDelay) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        // Delay must be different and not more than 5 days (to avoid costly mistakes)
+        if (_newDelay == delay || _newDelay > 5 days) {
             revert Errors.AccessControlDelayed_InvalidDelay();
         }
-        if (newDelay > delay) {
-            // new delay is longer, just set it
-            delay = newDelay;
+
+        if (block.timestamp < delayReductionLockTime) {
+            // The delay must already have been reduced because delayReductionLockTime is in the future
+            // We can't have set the delay to proposedDelay yet, so we can just delete it
+            delete proposedDelay;
+        }
+
+        if (_newDelay > delay) {
+            // New delay is longer, just set it
+            delay = _newDelay;
         } else {
-            // new delay is shorter, enforce old delay until it is met
-            delay = newDelay;
-            uint256 timeDelta = delay - newDelay;
-            // set delay reduction lock time to current time + timeDelta
-            // this is the timestamp when we can use the new delay
-            delayReductionLockTime = block.timestamp + timeDelta;
+            // New delay is shorter, enforce old delay until it is met
+            delayReductionLockTime = block.timestamp + delay;
+            proposedDelay = _newDelay;
         }
     }
 
-    function _checkDelay(bytes32 proposalId) internal view returns (bool) {
+    // an internal function that will return the timestamp to wait until,
+    // foctors in the the delayReuctionLockTime
+    // if after the lock time we can set delay to the new value
+    function _getDelayTimestamp() internal returns (uint256) {
+        if (block.timestamp < delayReductionLockTime) {
+            // We haven't reached the lock time yet,
+            // We must wait until the greater of the lock time, or now + proposedDelay
+            uint256 proposedDelayTime = block.timestamp + proposedDelay;
+            return proposedDelayTime > delayReductionLockTime ? proposedDelayTime : delayReductionLockTime;
+        }
+        // We have reached the lock time, we may set the delay to the proposed delay
+        if (proposedDelay != 0) {
+            delay = proposedDelay;
+            delete proposedDelay;
+        }
+        return block.timestamp + delay;
+    }
+
+    function _checkProposalWaitTime(bytes32 proposalId) internal view returns (bool) {
         return block.timestamp >= proposedRoles[proposalId];
     }
 
