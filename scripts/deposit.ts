1b5e7772b9091de7c4c6f6f1bddbabc93c2b257b
ba1ciu <jakub@amorphic.xyz>
2024-10-21 15:21:05 +0200

    Testnet setup script with setting up existing deposit for one account

--- scripts/deposit.ts
+++ scripts/deposit.ts
@@ -1,12 +1,8 @@
 import { executeSafeTransaction, getPortfolioUpdateTx, Pool, poolToProtocol, Portfolio, portfolioUpdateToBalanceUpdates, SafeOperation } from 'athena-sdk';
 import { Signer } from 'ethers';
 import { ethers } from 'hardhat';
-import { constants } from '../tests';
-import { approveTokenForSafe, deploySafeForSigner } from './safe-setup';
-import { deployAndFundTestnet } from './testnet-deploy-and-fund';
-import { withdraw } from './withdraw';
 
-async function deposit(signer: Signer, safeAddress: string, sequenceExecutorAddress: string, pool: Pool, amount: bigint, strategyId: number) {
+export async function deposit(signer: Signer, safeAddress: string, sequenceExecutorAddress: string, pool: Pool, amount: bigint, strategyId: number) {
 
     const protocol = poolToProtocol(pool);
     const targetPortfolio: Portfolio = {
@@ -32,17 +28,11 @@ async function deposit(signer: Signer, safeAddress: string, sequenceExecutorAddr
 }
 
 async function main() {
-    const [deployer, testAccount1] = await ethers.getSigners();
-    const contracts = await deployAndFundTestnet(deployer, [testAccount1]);
-    const safeAddress = await deploySafeForSigner(testAccount1, await contracts.baseSetup.safeProxyFactory.getAddress());
-    await approveTokenForSafe(testAccount1, safeAddress, constants.tokenConfig.USDC.address, ethers.MaxUint256);
-    await deposit(testAccount1, safeAddress, await contracts.sequenceExecutor.getAddress(), Pool.FluidUSDC, 1000000000n, 1);
-    await withdraw(testAccount1, safeAddress, await contracts.sequenceExecutor.getAddress(), Pool.FluidUSDC, 1000000000n, 1);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
