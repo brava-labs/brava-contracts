967b7eb2c6116660e4da9d5f6f8e8b5f0bae5c19
Splidge <anything@dchilvers.com>
2024-12-11 11:31:14 +0000

    remove unused errors

--- contracts/Errors.sol
+++ contracts/Errors.sol
@@ -17,9 +17,7 @@ contract Errors {
     // AdminVault errors
     error AdminVault_FeePercentageOutOfRange(uint256 _providedPercentage, uint256 _minAllowed, uint256 _maxAllowed);
     error AdminVault_InvalidFeeRange(uint256 _minFee, uint256 _maxFee);
-    error AdminVault_AlreadyInitialized();
     error AdminVault_NotInitialized();
-    error AdminVault_Unauthorized(address _caller, bytes32 _requiredRole);
     error AdminVault_DelayNotPassed(uint256 _currentTime, uint256 _requiredTime);
     error AdminVault_NotFound(string _entityType, bytes4 _entityId);
     error AdminVault_NotProposed();
