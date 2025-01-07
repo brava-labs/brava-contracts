f0e23e53eb227e976f58af3b04cc5b15a6d7f32b
Splidge <anything@dchilvers.com>
2025-01-07 09:01:25 +0000

    Add comment about deposit limit

--- contracts/actions/clearpool/ClearpoolSupply.sol
+++ contracts/actions/clearpool/ClearpoolSupply.sol
@@ -31,6 +31,7 @@ contract ClearpoolSupply is ERC4626Supply {
 
     /// @inheritdoc ERC4626Supply
     /// @dev Clearpool uses maximumCapacity() and poolSize() to determine the remaining deposit capacity
+    /// @dev However, if maximumCapacity is 0, then the pool has no limit on deposit capacity
     /// @param _poolAddress The pool address
     /// @return The maximum amount that can be deposited to the pool
     function _getMaxDeposit(address _poolAddress) internal view override returns (uint256) {
